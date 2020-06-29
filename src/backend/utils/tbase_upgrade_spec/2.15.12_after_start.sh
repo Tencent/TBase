4360b39b684291a520ca358761a1e3480e9d6577
branwu <branwu@tencent.com>
2020-06-29 14:49:03 +0800

    fix bugs on tbase online upgrade.ID80501281

--- src/backend/utils/tbase_upgrade_spec/2.15.12_after_start.sh
+++ src/backend/utils/tbase_upgrade_spec/2.15.12_after_start.sh
@@ -13,18 +13,39 @@ execSql()
 {
     local sql="$1"
     local db=$2
-    export LD_LIBRARY_PATH=${bin_dir}/lib:${LD_LIBRARY_PATH} && export PATH=${bin_dir}/bin:${PATH} && $bin_dir/bin/psql -h $host -p $port -d $db -U $user -t -c "$sql" | sed '/^\s*$/d'
+    local node_host="$3"
+    local node_port=$4
+    export LD_LIBRARY_PATH=${bin_dir}/lib:${LD_LIBRARY_PATH}
+    export PATH=${bin_dir}/bin:${PATH}
+    $bin_dir/bin/psql -h $node_host -p $node_port -d $db -U $user -t -c "$sql" | sed '/^\s*$/d'
 }
 
-dbs=$(execSql "select datname from pg_database where datname !='template0'" $dbname)
-for db in ${dbs}
-do
-        echo $db
-        execSql "create extension pg_stat_log" $db
+getSeg()
+{
+    line="$1"
+    segNum="$2"
+
+    seg=$(echo "$line" | awk -F'|' '{print $segNum}' "segNum=$segNum")
+    seg=$(echo $seg)
+    echo $seg
+}
+
+nodeinfo=$(execSql "select node_host, node_port from pgxc_node where node_type='C' order by node_port asc limit 1" $dbname $host $port)
+
+num=$(echo "$nodeinfo" | wc -l)
+for ((i=1; i<=num; ++i)); do
+    node_host=$(getSeg "$nodeinfo" 1)
+    node_port=$(getSeg "$nodeinfo" 2)
+    dbs=$(execSql "select datname from pg_database where datname !='template0'" $dbname $node_host $node_port)
+    for db in ${dbs}
+    do
+        execSql "create extension if not exists pg_stat_log" $db $node_host $node_port
         if [ $? -eq 0 ]
         then
-                echo "create pg_stat_log on $host:$port:$db success"
+                echo "create pg_stat_log on $node_host:$node_port:$db success"
         else
-                echo "create pg_stat_log on $host:$port:$db failed"
+                echo "create pg_stat_log on $node_host:$node_port:$db failed"
         fi
+    done
 done
+
