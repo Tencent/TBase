d72d845d02dd43ad0341e0c362fa94271ae86da3
JennyJennyChen <chenzaini@sina.com>
2020-06-29 14:38:59 +0800

    Enforce child constraints during COPY TO a partitioned table.

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -3565,7 +3565,8 @@ readnextline:
                     check_partition_constr = false;
 
                 /* Check the constraints of the tuple */
-                if (cstate->rel->rd_att->constr || check_partition_constr)
+				if (resultRelInfo->ri_RelationDesc->rd_att->constr ||
+                    check_partition_constr)
 					ExecConstraints(resultRelInfo, slot, estate, true);
 
 #ifdef _MLS_
