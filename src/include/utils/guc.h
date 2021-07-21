3bbe2f1ac440045b08fb348a8550f8c56e2124a8
arrowbowang <arrowbowang@tencent.com>
2021-07-21 17:16:49 +0800

    fix comments of mr

--- src/include/utils/guc.h
+++ src/include/utils/guc.h
@@ -310,7 +310,7 @@ extern int    tcp_keepalives_count;
 #ifdef _SHARDING_
 extern bool g_allow_dml_on_datanode;
 extern bool g_allow_force_ddl;
-extern bool g_parse_snapshot;
+extern bool g_snapshot_for_analyze;
 extern bool trace_extent;
 #endif
 
