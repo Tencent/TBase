dce74451dbd77ee61fc613d08ac40125e7021d01
arrowbowang <arrowbowang@tencent.com>
2022-04-01 11:25:02 +0800

    Revert "[BUGFIX] Subtransaction commits should not reset session information"
    This reverts commit 98634f5bec4daf68503344f3b251ee30b5cd9bbf.

--- src/backend/pgxc/pool/execRemote.c
+++ src/backend/pgxc/pool/execRemote.c
@@ -4850,12 +4850,14 @@ pgxc_node_remote_commit(TranscationType txn_type, bool need_release_handle)
     stat_transaction(conn_count);
 
     /* do not cleanup remote session for subtrans */
-	if (!temp_object_included && need_release_handle)
+	if (!temp_object_included)
         {
             /* Clean up remote sessions */
 		pgxc_node_remote_cleanup_all(txn_type == TXN_TYPE_CommitSubTxn ||
 		                             txn_type == TXN_TYPE_RollbackSubTxn);
 
+		if (need_release_handle)
+		{
 			if (PersistentConnections)
 			{
 				reset_handles();
@@ -4872,6 +4874,7 @@ pgxc_node_remote_commit(TranscationType txn_type, bool need_release_handle)
 			}
         }
     }
+	}
 
     clear_handles();
 }
@@ -5960,12 +5963,13 @@ pgxc_node_remote_abort(TranscationType txn_type, bool need_release_handle)
 	 * certain issues for aborted transactions, we drop the connections.
 	 * Revisit and fix the issue
 	 */
-	if (!temp_object_included && need_release_handle)
+	if (!temp_object_included)
         {
             /* Clean up remote sessions */
 		pgxc_node_remote_cleanup_all(txn_type == TXN_TYPE_CommitSubTxn ||
 		                             txn_type == TXN_TYPE_RollbackSubTxn);
-
+		if (need_release_handle)
+		{
 			if (HaveActiveDatanodeStatements())
 			{
 				reset_handles();
@@ -5975,6 +5979,7 @@ pgxc_node_remote_abort(TranscationType txn_type, bool need_release_handle)
 			release_handles(false);
         }
     }
+	}
     
     clear_handles();
     pfree_pgxc_all_handles(handles);
